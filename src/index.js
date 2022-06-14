a769f4c2e1cceece5e4204ba6a9f4160bd28ef10
mercywanjugu <mercy.wanjugu@student.moringaschool.com>
2022-06-14 07:47:14 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+    const newTaskPriority = document.getElementById("new-task-priority");
+    const newTaskUl = document.getElementById("tasks");
+    const newTaskForm = document.getElementById("create-task-form");
+    const newTaskDescription = document.getElementById("new-task-description"); 
+    newTaskForm.addEventListener("submit", createNewTask);
+  });
+  const appendNewTask = task => {
+    document.getElementById("tasks").appendChild(task);
+  }
+  const createNewTask = event => {
+    event.preventDefault();
+    const newTask = document.createElement("li");
+    const newTaskDescription = document.getElementById("new-task-description");
+    newTask.innerText = newTaskDescription.value;
+    appendNewTask(newTask);
+    event.target.reset();
+  };
